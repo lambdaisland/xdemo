7cef6705fb3ae0fb117b0a34fa2f5c7927264fb1
Arne Brasseur <arne@arnebrasseur.net>
2018-06-26 22:13:39 +0200

    finish recording episode

---
+++ src/lambdaisland/into_transducers.clj
@@ -0,0 +1,171 @@
+(ns lambdaisland.into-transducers
+  (:require [clojure.string :as str]))
+
+reduce
+
+(reduce + [1 2 3])
+;; => 6
+
+(reduce + 100 [1 2 3])
+;; => 106
+
+(reductions (fn [acc x]
+              (+ acc x))
+            0
+            [1 2 3])
+;; => (0 1 3 6)
+
+(reduce (fn [acc x]
+          (conj acc (* x x)))
+        []
+        [1 2 3])
+;; => [1 4 9]
+
+
+(defn my-into [target src]
+  (reduce conj target src))
+
+(my-into #{} [1 2 3])
+;; => #{1 3 2}
+
+(reduce (fn [acc x]
+          (if (> acc 5)
+            (reduced acc)
+            (+ acc x)))
+        [1 2 3 4 5])
+;; => 6
+
+(reduced 42)
+;; => #<Reduced@1d158391: 42>
+
+(type (reduced 42))
+;; => clojure.lang.Reduced
+
+@(reduced 42)
+;; => 42
+
+(type (reduced 42))
+;; => clojure.lang.Reduced
+
+(unreduced (reduced 42))
+;; => 42
+
+(unreduced (reduced 42))
+;; => 42
+
+["wulong" "red" "black" "green"]
+;;=> ["1. wulong" "2. red" "3. black" "4. green"]
+
+(second
+ (reduce (fn [[idx res] s]
+           [(inc idx) (conj res (str idx ". " s))])
+         [1 []]
+         ["wulong" "red" "black" "green"]))
+;; => ["1. wulong" "2. red" "3. black" "4. green"]
+
+
+(defn add-index-prefix
+  ([] [1 []])
+  ([acc] (second acc))
+  ([[idx res] x]
+   [(inc idx) (conj res (str idx ". " x))]))
+
+(transduce identity add-index-prefix ["wulong" "red" "black" "green"])
+;; => ["1. wulong" "2. red" "3. black" "4. green"]
+
+(transduce identity + [1 2 3 4])
+;; => 10
+
+(-)
+(transduce identity - [1 2 3 4])
+
+(transduce identity - 0 [1 2 3 4])
+;; => 10
+
+(transduce identity (completing - #(str "Result: " %)) 0 [1 2 3 4])
+;; => Result: -10
+
+(transduce identity (completing -) 0 [1 2 3 4])
+;; => -10
+
+
+(transduce identity
+           (completing
+            (fn [[idx res] s]
+              [(inc idx) (conj res (str idx ". " s))])
+            second)
+           [1 []]
+           ["wulong" "red" "black" "green"])
+;; => ["1. wulong" "2. red" "3. black" "4. green"]
+
+
+;; Transducer :: rf -> rf
+(fn [rf]
+  (fn
+    ([] (rf))
+    ([acc] (rf acc))
+    ([acc x] (rf acc x))))
+
+;; A :: rf -> rf
+;; B :: rf -> rf
+;;=>
+;; (comp A B) :: rf -> rf
+
+
+
+
+
+(require '[net.cgrand.xforms :as x])
+
+(x/count (filter #(= 0 (mod % 3))) (range 1 20))
+;; => 6
+
+(x/some (filter #(= 0 (mod % 3))) (range 1 20))
+;; => 3
+
+(x/str (comp (map str/upper-case)
+             (interpose "-"))
+       ["foo" "bar" "baz"])
+;; => FOO-BAR-BAZ
+
+(into [] x/str ["foo" "bar" "baz"])
+;; => ["foobarbaz"]
+
+(into [] (x/reduce +) [3 5 7])
+;; => [15]
+
+(into [] x/count [1 2 3])
+;; => [3]
+
+(into [] (x/into #{}) [1 2 3])
+;; => [#{1 3 2}]
+
+(into {}
+      (x/by-key :color :size (x/reduce +))
+      [{:color :blue, :size 1}
+       {:color :red, :size 2}
+       {:color :blue, :size 3}])
+;; => {:blue 4, :red 2}
+
+(require '[redux.core :as redux])
+
+
+(transduce identity
+           (redux/post-complete
+            + (fn [x] {:result x}))
+           [1 2 3])
+;; => {:result 6}
+
+(transduce identity
+           (redux/pre-step + :width)
+           [{:width 10} {:width 20}])
+;; => 30
+
+(transduce identity
+           (redux/fuse {:sum + :product *})
+           [1 2 3 4])
+;; => {:sum 10, :product 24}
+
+
+(x/transjuxt [x/min x/max (x/reduce +)] [1 10 100])
+;; => [1 100 111]
--- src/lambdaisland/xdemo/abalone.clj
+++ src/lambdaisland/xdemo/abalone.clj
@@ -2,7 +2,8 @@
   (:require [clojure.data.csv :as csv]
             [clojure.java.io :as io]
             [net.cgrand.xforms :as x]
-            [kixi.stats.core :as stats]))
+            [kixi.stats.core :as stats]
+            [com.hypirion.clj-xchart :as xchart]))
 
 ;; Demo of some basic linear regression using kixi.stats. Our dataset is the
 ;; Abalone dataset from the UCI Machine Learning Repository, found at
@@ -32,7 +33,6 @@
         (csv/read-csv (slurp (io/resource "abalone.data")))))
 
 ;; With a little bit of massaging our data is a nice vector of maps.
-
 data
 ;;=>
 [{:length 0.455,
@@ -44,6 +44,10 @@ data
   :shell-weight 0.15,
   :rings 15.0}
  ,,,]
+
+
+;;
+(x/transjuxt (into {} (map (juxt identity #(comp (map %) (x/reduce stats/summary)))) attrs) data)
 
 ;; Let's have a look at what we have
 (into []
@@ -98,6 +102,27 @@ data
 
 ;; checks out!
 
+;; This method of linear regression uses a least squares method.
+;; http://ib.berkeley.edu/courses/ib162/Regress.htm
+;; https://en.wikipedia.org/wiki/Simple_linear_regression
+
+;; Lets have a look at that data!
+
+(comment
+  (xchart/view
+   (xchart/xy-chart
+    {"Abalones" (xchart/extract-series
+                 {:x :diameter
+                  :y :rings}
+                 data)
+     "Linear model" {:x [0 1]
+                     :y [(first coefficients) (predict coefficients 1)]
+                     :style {:render-style :line
+                             :marker-type :none}}}
+    {:title "Abalone size and amount of rings"
+     :x-axis {:decimal-pattern "##.## mm"}
+     :render-style :scatter})))
+
 
 ;; In this case I picked diameter since it seemed intuitively to be a good
 ;; predictor of age, but lets have a look which other attributes could serve.
@@ -116,3 +141,141 @@ data
 ;;     [[:shell-weight :rings] 0.6275740445103175])
 
 ;; Seems shell-weight would be an even better predictor
+
+;; Notice that kixi.stats mostly provides reducing functions, and not
+;; transducers, this makes sense, since you're interested in a final result, not
+;; in a stream of data, but that does mean you end up using (transduce identity)
+;; a lot, since (reduce ) doesn't call the zero/one arity version of reducing
+;; functions.
+;;
+;; An alternative is xform's x/reduce and x/reductions, which will turn a
+;; reducing function into a transducer, which you can then use in any number of
+;; transducing contexts.
+
+(x/transjuxt {:arit (x/reduce stats/arithmetic-mean)
+              :geom (x/reduce stats/geometric-mean)
+              :harm (x/reduce stats/harmonic-mean)}
+             [1 2 3])
+;; => {:arit 2.0, :geom 1.8171205928321397, :harm 18/11}
+
+;; x/reductions is also cool because it allows you to see how some of these
+;; statistical functions iteratively approach their final result. For example,
+;; here's a sequence of [offset slope] coefficients that gradually approach the
+;; value we calculated earlier as we feed it more data into.
+;;
+;; This partly duplicates code from kixi.stats.core/simple-linear-regression.
+;; That function is really just sum-squares plus some completing logic.
+;; x/reductions will never call the completing step, but we want to call it on
+;; every step.
+
+(sequence
+ (comp (x/reductions (stats/sum-squares :shell-weight :rings))
+       (map (fn [[_ x-bar y-bar ss-x _ ss-xy]] ; x-bar = arithmetic mean of x
+                                        ; ss-x = sum of squared residuals (errors) for x
+                                        ; ss-xy = "Corrected sum of cross products"
+              (when-not (zero? ss-x)
+                (let [b (/ ss-xy ss-x)]
+                  [(- y-bar (* x-bar b)) b])))))
+ data)
+;;=>
+'(nil
+  nil
+  [-1.7763568394002505E-15 100.00000000000001]
+  [7.621621621621621 18.918918918918926]
+  [7.554231974921629 18.432601880877748]
+  [6.359591100420925 25.315694527961522]
+  [6.055944055944053 25.874125874125884]
+  [4.013567219405232 43.949568315746205]
+  [3.943723382680994 44.77793551003854]
+  [3.66337748941155 44.90402811570696]
+  [3.465893567286967 46.50739200388574]
+  [3.4942490962865573 46.730200460072304]
+  [3.5302616165560305 46.622413119875056]
+  [3.4462452728254984 46.497388798847474]
+  [3.367872955164314 45.76690432143674]
+  [3.249485896092809 45.74554766616227]
+  [3.357097636465955 44.42881260551491]
+  [3.0753660907466074 45.49559436831708]
+  [3.22640042613841 44.96911936194412]
+  [3.093919958953311 45.50538737814264]
+  [3.1911986430456096 45.137580098002275]
+  [4.00477154424523 41.5558126084442]
+  [4.3811947645150475 39.94854010515719]
+  [4.65119330763553 37.53793160009843]
+  [4.638914041277673 36.837668217441546]
+  [5.288535741278493 31.805826272591066]
+  [5.639811251762987 29.04470872293129]
+  [5.838915168716496 27.37937559129612]
+  [5.926011739264754 26.629595304294096]
+  [5.844888841986777 27.27240748557759]
+  [5.837301199495221 27.25994161861745]
+  [6.272493254466336 24.248596402883095]
+  [6.646372177690841 22.049822813429046]
+  [6.402265054447462 23.745504452970813]
+  [6.198695715890773 24.931203491117515]
+  [6.787252808008873 21.61302955608284]
+  [6.674423264471025 21.93606559686668]
+  [6.619002100871374 22.40781725078376]
+  [6.500496806245564 22.719659332860182]
+  [6.489200101490363 22.608633299870927]
+  [6.529334301119866 22.480298631273328]
+  [6.521363155082329 22.504106633945533]
+  [6.523005366898163 22.64929500166327]
+  [6.323383896757455 23.36713660395757]
+  [6.171375868245412 23.918787218965154]
+  [5.95362910747963 24.706097288065056]
+  [5.902989991453902 24.869717459237183]
+  [5.887333290564862 24.906884232396678]
+  [5.791367685166282 25.113509545901675]
+  [5.730707958163569 25.32620929786196]
+  [5.713573811950107 25.11594107439006]
+  [5.659188779529255 25.144524386623786]
+  [5.603767504173028 25.312379854869057]
+  [5.613829714305395 25.294285106105626]
+  [5.632350979855261 25.255368273845313]
+  [5.573639060020683 25.42699364903863]
+  [5.541219361550605 25.301998313133584]
+  [5.509057482844197 25.382005430778]
+  [5.466482097723363 25.44003221831009]
+  [5.339297504382814 25.924593813708974]
+  [5.280912093516649 25.979974773803306]
+  [5.226762276853849 26.112202244210106]
+  [5.211279502704019 26.126261247623003]
+  [5.205748957427247 26.11881471281976]
+  [5.1747294027076896 26.216670987823317]
+  [5.142838701645983 26.220639134522656]
+  [5.123643250429573 26.26832725037938]
+  [5.151967711201643 25.99571773381243]
+  [5.404699027644314 24.31496255802915]
+  [5.4137043486713 24.302895587939847]
+  [5.390120329003577 24.38905793925179]
+  [5.38594359656925 24.477596858609534]
+  [5.391509560871014 24.46041705996095]
+  [5.244542417233772 25.52220036540184]
+  [5.3400348340604396 24.7604084057243]
+  [5.341519329492499 24.743417775314057]
+  [5.332065447045879 24.97262085628453]
+  [5.375209754351952 24.483065285438077]
+  [5.398139485712888 24.18747176322173]
+  [5.403275044863871 24.081072684036755]
+  [5.398805134964131 24.12073321487818]
+  [5.4103628370414585 23.866880364228802]
+  [6.036700667893908 20.33902446732918]
+  [6.015754957670198 20.63448475356282]
+  [5.8821601693336305 21.41865648377899]
+  [5.878070895636839 21.45941426099632]
+  [5.948876972985198 20.963052187755085]
+  [5.96865443750676 20.805241718081934]
+  [5.960578697407513 20.77122264505984]
+  [5.9529414432511505 20.785977734842163]
+  [5.956665402001267 20.728923718172535]
+  [5.980676064565406 20.806131395126826]
+  [5.984082387161655 20.8204983943922]
+  [5.99867601545931 20.70338918790904]
+  [6.051038713120027 20.283195261676912]
+  [6.073495754401115 20.145162768677793]
+  [6.11435136959475 19.891930471969612]
+  [6.097637711172138 19.871326231823122]
+  [6.076021111381374 19.921374405263116]
+  [6.0308523925829585 20.013326928932806]
+  ...)
